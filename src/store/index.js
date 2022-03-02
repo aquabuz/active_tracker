f6136ecf665b5d80982c5426dad04afb75f184a1
EastHill <aquabuz@gmail.com>
2022-03-02 23:47:05 +0900

    create start

--- src/store/index.js
+++ src/store/index.js
@@ -1,1 +1,16 @@
+import { reactive } from "vue";
 
+const state = reactive({
+  user: null,
+});
+
+const methos = {
+  setUser(payload) {
+    state.user = payload ? payload.user : null;
+  },
+};
+
+export default {
+  state,
+  methos,
+};
